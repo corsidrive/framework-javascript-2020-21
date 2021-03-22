58b6ecf9a3ad8cf4bbef35fbf7c9ff1f8e3789c0
corsidrive <corsidrive@gmail.com>
2021-03-22 09:31:57 +0100

    Funzione di ordine superiore

--- ES6/app.js
+++ ES6/app.js
@@ -3,14 +3,29 @@
 const trovaDispari = numero => numero % 2 !== 0
 
 const numeri = [4,7,5,4,2,8,9,9,2,1,]
-console.log(numeri);
+//console.log(numeri);
 
 let dispari = numeri.filter(trovaDispari) 
-console.log("trova dispari function expression",dispari);
+//console.log("trova dispari function expression",dispari);
 
 dispari = numeri.filter(numero => numero % 2 !== 0) 
-console.log("trova dispari ma come funzione anonima",dispari);
+//console.log("trova dispari ma come funzione anonima",dispari);
 
+const parole = ['latte','casa','mario','mela'];
+// javascript 2015 
+// const findWord = word => item => item === word;
+
+// javascript 2009 - 2014
+function findWord(word){
+    return function (item) {
+        return item === word
+    }
+}
+
+
+const result = parole.filter(findWord('mela'));
+
+console.log(result);
 /**
 numeri.filter(function(numero){
     return numero % 2 !== 0
--- README.md
+++ README.md
@@ -1,22 +1,19 @@
 # ES6
 
-
-## ES6 module
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
-
-
-Siti utili
-
+## Siti utili
 https://javascript.info/
 
-Array in js
-
+## Array in js
 https://javascript.info/array
 
-Map 
+## ES6 module
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Module
+
+
+## Map
 https://javascript.info/array-methods#transform-an-array
 
-function 
+## function 
 https://javascript.info/function-expressions
 
 https://javascript.info/arrow-functions-basics
